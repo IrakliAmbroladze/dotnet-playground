86a6f24c884ce4822b492d8626da0de22511514c
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-06-12 22:55:38 +0400

    feat: create method for multiplication

--- Program.cs
+++ Program.cs
@@ -7,25 +7,18 @@ namespace HomeWork_9_10
     {
         static void Main(string[] args)
         {
-            int[] arr = {33, 71, 5, 88, 42, 19, 65 };
-            int resultFirst = Array.Find(arr, x => x > 40);
-            int resultLast = Array.FindLast(arr, x => x > 40);
-            int[] resultAll = Array.FindAll(arr, x => x > 40);
-
-            Console.WriteLine($"First x > 40: {resultFirst}");
-            Console.WriteLine();
-            Console.WriteLine($"Last x > 40: {resultLast}");
-            Console.WriteLine();
-            Console.WriteLine("All x > 40:");
-
-            foreach (int num in resultAll)
-            {
-                Console.Write(num + " ");
-            }
-
-            Console.WriteLine();
+            int num1 = 5;
+            int num2 = 7;
 
 
+            int result = Multiply(num1, num2);
+
+            Console.WriteLine($"{num1} * {num2} = {result}");
+
+        }
+
+        static int Multiply(int a, int b) {
+            return a * b;
         }
     }
 }
