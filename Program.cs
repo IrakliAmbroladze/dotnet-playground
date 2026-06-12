602bce64292723e7f1f56a29c7e9a3bfb395954f
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-06-12 23:09:04 +0400

    feat: create isEven function

--- Program.cs
+++ Program.cs
@@ -7,23 +7,21 @@ namespace HomeWork_9_10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Sum(2,7));
-            Console.WriteLine(Sum(2.5,7));
-            Console.WriteLine(Sum(2,7,6));
+            int number = 5;
+            Console.WriteLine($"{number} is even: ");
+            Console.WriteLine(IsEven(number));
+            Console.WriteLine();
+
+            int number2 = 10;
+            Console.WriteLine($"{number2} is even: ");
+            Console.WriteLine(IsEven(number2));
+            Console.WriteLine();
 
         }
 
-        static int	Sum(int a, int b)
+        static bool IsEven(int number)
         {
-            return a + b;
-        } 
-        static double Sum(double a, double b)
-        {
-            return a + b;
-        }
-        static int Sum(int a, int b, int c)
-        {
-            return a + b + c;
+            return number % 2 == 0;
         }
 
     }
