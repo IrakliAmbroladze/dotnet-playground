de2b2fae74b809dfd2a6c3458715a13c1c0f4d36
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-06-26 22:35:09 +0400

    feat: demostration overflow exception

--- Program.cs
+++ Program.cs
@@ -2,25 +2,22 @@
 {
     internal class Program
     {
-
-
-
         static void Main(string[] args)
         {
-            int number = 8;
-            int result = Factorial(number);
+            byte a = 1, b = 8;
+            try
+            {
 
-            Console.WriteLine($"Factorial of {number} is {result}");
-            Console.WriteLine($"Factorial of 0 is {Factorial(0)}");
-            Console.WriteLine($"Factorial of 1 is {Factorial(1)}");
+                checked
+                {
+                    Console.WriteLine((byte)(a - b));
+                };
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("We got Overflow exception");
+            }
         }
 
-        static int Factorial(int number)
-        {
-            if (number == 0 || number == 1)
-                return 1;
-
-            return number * Factorial(number - 1);
-        }
     }
 }
