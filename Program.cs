3fe8ae57971740c8a98a3af9d3e1eaecb7346d4e
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-06-30 20:30:17 +0400

    create Point class

--- Program.cs
+++ Program.cs
@@ -2,6 +2,21 @@
 
 namespace Georgia
 {
+    class Point
+    {
+        public int x;
+        private int y;
+
+        private void PrintX()
+        {
+            Console.WriteLine(x);
+        }
+
+        private void PrintY()
+        {
+            Console.WriteLine(y);
+        }
+    }
     internal class Program
     {
 
