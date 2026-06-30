cbe69b171e8e30c4521ae8190f0883b7f697ca0b
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-06-30 19:22:11 +0400

    use System namespace directly in code

--- Program.cs
+++ Program.cs
@@ -1,27 +1,11 @@
-﻿namespace HomeWork_9_10
+﻿namespace Georgia
 {
     internal class Program
     {
-        enum Season : byte
-        {
-            Winter = 1, Spring, Summer, Autumn
-        }
 
         static void Main()
         {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-
-            Console.Write("შემოიტანეთ სეზონის სახელი (მაგ. summer, WINTER): ");
-            string input = Console.ReadLine();
-
-            if (Enum.TryParse(input, true, out Season parsedSeason))
-            {
-                Console.WriteLine($"სეზონი ცნობილია: {parsedSeason}");
-            }
-            else
-            {
-                Console.WriteLine("შეცდომა: ასეთი სეზონი არ არსებობს");
-            }
+            System.Console.WriteLine(107);
         }
 
     }
