2701bbe63744a7fad2ba9c4d39c51b33fb08ff0f
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-07-02 20:40:14 +0400

    compare struct and class

--- Program.cs
+++ Program.cs
@@ -1,25 +1,46 @@
 ﻿namespace Georgia
 {
-    class Item
+    struct Point
     {
-        public string description { get; set; }
-        public int count { get; set; }
-
-        public void PrintDefault()
-        {
-            Console.WriteLine(this.count);
-            Console.WriteLine(this.description);
-        }
+        public int X, Y;
     }
+
+    class SecondPoint
+    {
+        public int X, Y;
+        public SecondPoint(int x, int y) { X = x; Y = y; }
+    }
+
     class Program
     {
 
         static void Main()
         {
-            Item item = new Item();
+            Point point;
+            point.X = 8;
+            point.Y = 10;
 
-            item.PrintDefault();
 
+
+            Point testPoint = point;
+            testPoint.X = 20;
+            testPoint.Y = 21;
+            Console.WriteLine(point.X);
+            Console.WriteLine(point.Y);
+            Console.WriteLine(testPoint.X);
+            Console.WriteLine(testPoint.Y);
+
+            SecondPoint point2 = new SecondPoint(4, 5);
+            Console.WriteLine(point2.X);
+            Console.WriteLine(point2.Y);
+            SecondPoint testPoint2 = point2;
+            testPoint2.X = 40;
+            testPoint2.Y = 41;
+
+            Console.WriteLine(point2.X);
+            Console.WriteLine(point2.Y);
+            Console.WriteLine(testPoint2.X);
+            Console.WriteLine(testPoint2.Y);
 
         }
 
