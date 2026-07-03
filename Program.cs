8b5f058e42809495926b866bbead77e9b7072c71
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-07-03 19:08:40 +0400

    feat: compare structs and classes

--- Program.cs
+++ Program.cs
@@ -1,48 +1,28 @@
-﻿namespace Georgia
+﻿namespace University
 {
     struct Point
     {
         public int X, Y;
-    }
 
-    class SecondPoint
-    {
-        public int X, Y;
-        public SecondPoint(int x, int y) { X = x; Y = y; }
+        public double Distance(Point other)
+        {
+            int dx = other.X - X;
+            int dy = other.Y - Y;
+
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
     }
 
     class Program
     {
-
         static void Main()
         {
-            Point point;
-            point.X = 8;
-            point.Y = 10;
+            Point p1 = new Point { X = -1, Y = 3 };
+            Point p2 = new Point { X = 2, Y = 7 };
 
-
-
-            Point testPoint = point;
-            testPoint.X = 20;
-            testPoint.Y = 21;
-            Console.WriteLine(point.X);
-            Console.WriteLine(point.Y);
-            Console.WriteLine(testPoint.X);
-            Console.WriteLine(testPoint.Y);
-
-            SecondPoint point2 = new SecondPoint(4, 5);
-            Console.WriteLine(point2.X);
-            Console.WriteLine(point2.Y);
-            SecondPoint testPoint2 = point2;
-            testPoint2.X = 40;
-            testPoint2.Y = 41;
-
-            Console.WriteLine(point2.X);
-            Console.WriteLine(point2.Y);
-            Console.WriteLine(testPoint2.X);
-            Console.WriteLine(testPoint2.Y);
-
+            double distance = p1.Distance(p2);
+            Console.WriteLine(distance);
+            Console.WriteLine("some text");
         }
-
     }
 }
