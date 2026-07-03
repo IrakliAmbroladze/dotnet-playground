8250255704e22de2fe7528b96fc8b7a243ca1677
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-07-03 22:06:11 +0400

    feat: create private and public access modifiers

--- Program.cs
+++ Program.cs
@@ -1,14 +1,31 @@
-﻿using PersonLib;
-
-namespace Georgia
+﻿namespace Georgia
 {
+    class Point
+    {
+        public int X;
+        private int Y = 12;
+
+        private void PrintX()
+        {
+            Console.WriteLine($"X is {X}");
+        }
+
+        public void PrintY()
+        {
+            Console.WriteLine($"Y is {Y}");
+        }
+
+    }
     internal class Program
     {
 
         static void Main()
         {
-            Person person = new Person("Irakli", 35);
-            person.PrintInfo();
+            Point p = new Point();
+            p.X = 10;
+            Console.WriteLine($"print {p.X}");
+
+            p.PrintY();
         }
 
     }
