2898f1668c4dec7dab96c3f94729095949a467c7
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-07-08 00:53:08 +0400

    feat: check whether number is prime or composite

--- Program.cs
+++ Program.cs
@@ -1,18 +1,32 @@
 ﻿namespace PersonLib
 {
-
     internal class Program
     {
-
         static void Main()
         {
-            int value;
             int n = int.Parse(Console.ReadLine());
-            for (int i = 1; i <= 10; i++)
-            {
-                Console.WriteLine($"{i} * {n} = {i * n}");
-            }
-        }
 
+            if (n < 1)
+            {
+                Console.WriteLine("n must be greater than 1");
+                return;
+            }
+
+            bool isMartivi = true;
+
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    isMartivi = false;
+                    break;
+                }
+            }
+
+            if (isMartivi)
+                Console.WriteLine($"{n} is Martivi");
+            else
+                Console.WriteLine($"{n} is Rtuli");
+        }
     }
 }
