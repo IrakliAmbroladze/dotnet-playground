ba88f1f3cd3d80ce5da2b076cf86a8edb77a8c92
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-07-09 19:25:07 +0400

    feat: create static method of square

--- Program.cs
+++ Program.cs
@@ -2,31 +2,16 @@
 {
     internal class Program
     {
+        static int Square(int x)
+        {
+
+            return x * x;
+        }
         static void Main()
         {
-            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Square(4));
+            Console.WriteLine(Square(5));
 
-            if (n < 1)
-            {
-                Console.WriteLine("n must be greater than 1");
-                return;
-            }
-
-            bool isMartivi = true;
-
-            for (int i = 2; i < n; i++)
-            {
-                if (n % i == 0)
-                {
-                    isMartivi = false;
-                    break;
-                }
-            }
-
-            if (isMartivi)
-                Console.WriteLine($"{n} is Martivi");
-            else
-                Console.WriteLine($"{n} is Rtuli");
         }
     }
 }
