89d9ed7b25fd6d345a7167bc08c8696c33496679
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-07-11 13:49:49 +0400

    feat: create operations on fraction class

--- Program.cs
+++ Program.cs
@@ -2,41 +2,65 @@
 {
     internal class Program
     {
-        class Vector2D
+        class Fraction
         {
-            double X, Y;
-            public static Vector2D operator +(Vector2D v1, Vector2D v2)
+            int Numerator;
+            int Denominator;
+            public static Fraction operator *(Fraction a, Fraction b)
             {
-                return new Vector2D(v1.X + v2.X, v1.Y + v2.Y);
+                return new Fraction(a.Numerator * b.Numerator, a.Denominator * b.Denominator);
             }
-            public static Vector2D operator -(Vector2D v1, Vector2D v2)
+            public static Fraction operator /(Fraction a, Fraction b)
             {
-                return new Vector2D(v1.X - v2.X, v1.Y - v2.Y);
+                if (b.Numerator == 0) throw new ArgumentException("can't devide by zero");
+                return new Fraction(a.Numerator * b.Denominator, a.Denominator * b.Numerator);
+            }
 
-            }
-            public static Vector2D operator -(Vector2D v1)
+            public static bool operator ==(Fraction a, Fraction b)
             {
-                return new Vector2D(-v1.X, -v1.Y);
+                return a.Numerator * b.Denominator == a.Denominator * b.Numerator;
+            }
+
+            public static bool operator !=(Fraction a, Fraction b)
+            {
+                return a.Numerator * b.Denominator != a.Denominator * b.Numerator;
+            }
+            public override bool Equals(object? obj)
+            {
+                if (obj is Fraction other)
+                {
+                    return this == other;
+                }
+
+                return false;
+            }
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(Numerator, Denominator);
+            }
+            public Fraction(int numerator, int denominator)
+            {
+                if (denominator == 0)
+                    throw new ArgumentException("Denominator cannot be zero.");
+                Numerator = numerator;
+                Denominator = denominator;
             }
             public override string ToString()
             {
-                return ($"({X},{Y})");
+                if (Numerator == Denominator)
+                {
+                    return "1";
+                }
+                return $"{Numerator}/{Denominator}";
             }
-            public Vector2D(double x, double y)
-            {
-                X = x;
-                Y = y;
-            }
-
         }
         static void Main()
         {
-            Vector2D v1 = new Vector2D(3, 4);
-            Vector2D v2 = new Vector2D(1, 2);
-            Console.WriteLine((v1 + v2).ToString());
-            Console.WriteLine((v1 - v2).ToString());
-            Console.WriteLine((-v1).ToString());
-
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(2, 4);
+            Console.WriteLine($"does {f1} equal to {f2}? {f1 == f2}");
+            Console.WriteLine($"{f1} x {f2} = {f1 * f2}");
+            Console.WriteLine($"{f1} / {f2} = {f1 / f2}");
         }
     }
 }
