d172ca3b5e7062b05cdd955beb5d0a62662573ef
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-07-11 10:32:58 +0400

    feat: create class for Circle getArea

--- Program.cs
+++ Program.cs
@@ -2,49 +2,24 @@
 {
     internal class Program
     {
-        class BankAccount
+        class Circle
         {
-            private decimal balance;
-            public decimal Balance
-            {
-                get { return balance; }
-                set
-                {
-                    if (value >= 0)
-                    {
-                        balance = value;
-                    }
-                }
-            }
-            public void Deposit(decimal amount)
-            {
-                if (amount > 0)
-                {
-                    Balance += amount;
-                }
-            }
-            public void Withdraw(decimal amount)
-            {
-                if (amount > 0 && Balance - amount >= 0)
-                {
-                    Balance -= amount;
-                }
-                else { Console.WriteLine("Not enough amount on deposit"); }
-            }
-        }
+            const double Pi = 3.14159;
+            public readonly double Radius;
 
+            public double GetArea() { return Pi * Radius * Radius; }
+
+            public Circle(double radius)
+            {
+                Radius = radius;
+            }
+
+        }
         static void Main()
         {
-            BankAccount account = new BankAccount() { Balance = 500 };
-            Console.WriteLine($"Starting Balance: {account.Balance}");
-            Console.WriteLine();
+            Circle circle = new Circle(5);
+            Console.WriteLine($"The area of the circle with radius {circle.Radius} is {circle.GetArea()}");
 
-            int withdraw = 700;
-            Console.WriteLine($"Try to withdraw {withdraw}");
-            account.Withdraw(withdraw);
-
-            Console.WriteLine();
-            Console.WriteLine($"Final Balance: {account.Balance}");
         }
     }
 }
