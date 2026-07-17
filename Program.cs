163f17942cb4641985d651f7cbc942401c2e9928
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-07-17 22:54:48 +0400

    feat: make math calculations on user inputs

--- Program.cs
+++ Program.cs
@@ -1,53 +1,44 @@
 ﻿namespace Georgia
 {
-
-    class Book
-    {
-        public string Title = string.Empty;
-        public string Author = string.Empty;
-
-        public override string ToString()
-        {
-            return $"{Title} - {Author}";
-        }
-        public override bool Equals(object? obj)
-        {
-            if (obj is Book)
-            {
-                Book other = (Book)obj;
-                return Title == other.Title &&
-                       Author == other.Author;
-            }
-
-            return false;
-        }
-        public override int GetHashCode()
-        {
-            return Title.GetHashCode();
-        }
-
-    }
     internal class Program
     {
         static void Main()
         {
-            Book bookOne = new Book() { Title = "Iliad", Author = "Homer" };
-            Book bookTwo = new Book() { Title = "Iliad", Author = "Homer" };
-            Book bookThree = new Book() { Title = "Ulysses", Author = "James Joyce" };
-
-            Console.WriteLine(bookOne);
-            Console.WriteLine(bookTwo);
-            Console.WriteLine(bookThree);
-
-            Console.WriteLine($"Compare bookOne to bookTwo {bookOne.Equals(bookTwo)}");
-            Console.WriteLine($"Compare bookOne to bookThree {bookOne.Equals(bookThree)}");
-            Console.WriteLine($"Compare bookTwo to bookThree {bookTwo.Equals(bookThree)}");
+            Console.WriteLine("The following program makes math operations on two numbers.");
             Console.WriteLine();
+            Console.Write("Enter number one: ");
+            string inputOne = Console.ReadLine();
+            Console.Write("Enter number two: ");
+            string inputTwo = Console.ReadLine();
 
-            Console.WriteLine($"bookOne hashCode {bookOne.GetHashCode()}");
-            Console.WriteLine($"bookTwo hashCode {bookTwo.GetHashCode()}");
-            Console.WriteLine($"bookThree hashCode {bookThree.GetHashCode()}");
+            try
+            {
+                double numberOne = Convert.ToDouble(inputOne);
+                double numberTwo = Convert.ToDouble(inputTwo);
+                Console.WriteLine();
 
+                Console.WriteLine($"{numberOne} + {numberTwo} = {numberOne + numberTwo}");
+                Console.WriteLine($"{numberOne} - {numberTwo} = {numberOne - numberTwo}");
+                Console.WriteLine($"{numberOne} x {numberTwo} = {numberOne * numberTwo}");
+
+                if (numberTwo == 0) { throw new DivideByZeroException(); }
+                Console.WriteLine($"{numberOne} / {numberTwo} = {numberOne / numberTwo}");
+
+                Console.WriteLine($"{numberOne} ^ {numberTwo} = {Math.Pow(numberOne, numberTwo)}");
+                Console.WriteLine($"Calculations performed at {DateTime.Now}");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Cannot divide by zero.");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Please enter valid numbers.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unknown error.");
+            }
         }
     }
 }
