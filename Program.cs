defad2f8674a8f55477e131c5e92939292187bde
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-07-17 22:22:33 +0400

    feat: override object methods

--- Program.cs
+++ Program.cs
@@ -1,39 +1,53 @@
 ﻿namespace Georgia
 {
+
+    class Book
+    {
+        public string Title = string.Empty;
+        public string Author = string.Empty;
+
+        public override string ToString()
+        {
+            return $"{Title} - {Author}";
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj is Book)
+            {
+                Book other = (Book)obj;
+                return Title == other.Title &&
+                       Author == other.Author;
+            }
+
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return Title.GetHashCode();
+        }
+
+    }
     internal class Program
     {
         static void Main()
         {
-            int a = 100;
-            object boxedInt = a;
-            Console.WriteLine("Boxed int:");
-            Console.WriteLine(boxedInt.GetType());
-            int backToInt = (int)boxedInt;
-            Console.WriteLine();
-            Console.WriteLine($"initial value is {a}");
-            Console.WriteLine($"last value is {backToInt}");
+            Book bookOne = new Book() { Title = "Iliad", Author = "Homer" };
+            Book bookTwo = new Book() { Title = "Iliad", Author = "Homer" };
+            Book bookThree = new Book() { Title = "Ulysses", Author = "James Joyce" };
+
+            Console.WriteLine(bookOne);
+            Console.WriteLine(bookTwo);
+            Console.WriteLine(bookThree);
+
+            Console.WriteLine($"Compare bookOne to bookTwo {bookOne.Equals(bookTwo)}");
+            Console.WriteLine($"Compare bookOne to bookThree {bookOne.Equals(bookThree)}");
+            Console.WriteLine($"Compare bookTwo to bookThree {bookTwo.Equals(bookThree)}");
             Console.WriteLine();
 
-            bool boolValue = true;
-            object boxedBoolValue = boolValue;
-            Console.WriteLine("Boxed bool:");
-            Console.WriteLine(boxedBoolValue.GetType());
-            Console.WriteLine();
-
-            decimal decimalValue = 10.5m;
-            object boxedDecimalValue = decimalValue;
-            Console.WriteLine("Boxed decimal:");
-            Console.WriteLine(boxedDecimalValue.GetType());
+            Console.WriteLine($"bookOne hashCode {bookOne.GetHashCode()}");
+            Console.WriteLine($"bookTwo hashCode {bookTwo.GetHashCode()}");
+            Console.WriteLine($"bookThree hashCode {bookThree.GetHashCode()}");
 
         }
     }
 }
-
-
-
-
-
-
-
-
-
