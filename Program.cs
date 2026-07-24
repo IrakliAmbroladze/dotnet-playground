bbfe178a29f27786935b5a1a2c02a069d8427f27
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-07-24 21:20:36 +0400

    refactor: use Func instead of custom delegate

--- Program.cs
+++ Program.cs
@@ -2,15 +2,13 @@
 {
     internal class Program
     {
-        public delegate string Reverse(string s);
-
         static string ReverseString(string s)
         {
             return new string(s.Reverse().ToArray());
         }
         static void Main(string[] args)
         {
-            Reverse rev = ReverseString;
+            Func<string, string> rev = ReverseString;
             Console.WriteLine(rev("a string"));
 
         }
