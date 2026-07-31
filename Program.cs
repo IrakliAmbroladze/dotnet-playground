911a3162d0da3cb1c3da097628a006dd191d9d85
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-07-31 21:18:51 +0400

    feat: create Interface for Car and Motorcycle classes

--- Program.cs
+++ Program.cs
@@ -2,14 +2,41 @@
 {
     class Program
     {
+        interface IVehicle
+        {
+            string Brand { get; set; }
+            void Start();
+            double CalculateFuelCost(double distanceKm, double consumptionPerKm);
+        }
+        class Car : IVehicle
+        {
+            public string Brand { get; set; } = string.Empty;
+            public void Start() { Console.WriteLine($"{Brand} car has started!"); }
+            public double CalculateFuelCost(double distanceKm, double consumptionPerKm)
+            {
+                return distanceKm * consumptionPerKm * 2.5;
+            }
+        }
+        class Motorcycle : IVehicle
+        {
+            public string Brand { get; set; } = string.Empty;
+            public void Start() { Console.WriteLine($"{Brand} motorcycle  has started!"); }
+            public double CalculateFuelCost(double distanceKm, double consumptionPerKm)
+            {
+                return distanceKm * consumptionPerKm * 1.5;
+            }
+        }
+
         static void Main(string[] args)
         {
-
-            Func<int, int, string> compareNumbers = (int x, int y) => (x > y) ? "First is greater" : (x < y) ? "Second is greater" : "Numbers are equal";
-
-            Console.WriteLine(compareNumbers(7, 5));
-            Console.WriteLine(compareNumbers(2, 5));
-            Console.WriteLine(compareNumbers(5, 5));
+            Car car = new Car() { Brand = "Toyota" };
+            Motorcycle motorcycle = new Motorcycle() { Brand = "Yamaha" };
+            IVehicle[] vehicles = new IVehicle[] { car, motorcycle };
+            foreach (IVehicle vehicle in vehicles)
+            {
+                vehicle.Start();
+                Console.WriteLine($"Fuel cost: {vehicle.CalculateFuelCost(100, 0.7)}");
+            }
         }
     }
 }
