88e184fd45ed394d3d3f38bd1b0891af118a5aad
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-07-31 22:24:54 +0400

    feat: manipulate Link Capacity and Count

--- Program.cs
+++ Program.cs
@@ -2,34 +2,33 @@
 {
     class Program
     {
-        interface IRepository<T>
-        {
-            void Add(T item);
-        }
-        static void AddItem<T>(T item, IRepository<T> repository) where T : class { repository.Add(item); }
-        class StringRepository : IRepository<string>
-        {
-            List<string> items = new List<string>();
-            public void Add(string item)
-            {
-                items.Add(item);
-                Console.WriteLine($"{item} added.");
-                Console.WriteLine("List is: ");
-                foreach (var item2 in items)
-                {
-                    Console.WriteLine(item2);
-                }
-                Console.WriteLine("------------");
-
-            }
-
-        }
         static void Main(string[] args)
         {
-            StringRepository repository = new StringRepository();
+            Random random = new Random();
+            int size = 10;
+            List<int> list = new List<int>();
+            for (int i = 0; i < size; i++)
+            {
+                list.Add(random.Next(1, 100));
+                Console.WriteLine($"Capacity {list.Capacity}");
+                Console.WriteLine($"Count {list.Count}");
+            }
+            Console.WriteLine();
+            list.Sort();
+            Console.Write("List numbers: ");
 
-            AddItem("Hello", repository);
-            AddItem("World", repository);
+            foreach (int i in list) { Console.Write($"{i}, "); }
+            List<int> evenList = list.FindAll(x => x % 2 == 0);
+
+            Console.WriteLine();
+
+            Console.Write("Even numbers: ");
+            foreach (int i in evenList) { Console.Write($"{i}, "); }
+            Console.WriteLine();
+            list.TrimExcess();
+            Console.WriteLine($"Capacity after TrimExcess {list.Capacity}");
+            Console.WriteLine($"Count after TrimExcess {list.Count}");
+
         }
     }
 }
