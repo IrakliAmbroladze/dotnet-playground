6f0bc6767c626a84e6a35fbd3680aaf0defbb28f
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-08-06 20:50:58 +0400

    feat: create threads

--- Program.cs
+++ Program.cs
@@ -1,11 +1,37 @@
-﻿namespace Georgia
+﻿Thread odd = new(OddThread);
+Thread even = new(EvenThread);
+
+odd.Start();
+even.Start();
+
+odd.Join();
+even.Join();
+
+Console.WriteLine("dasasruli");
+static void OddThread()
 {
-    partial class Program
+    for (int i = 0; i <= 15; i++)
     {
-        static void Main(string[] args)
+        if (i % 2 == 0)
         {
-            Student student = new Student("Irakli", "Ambroladze", 35, 2.50);
-            student.ShowGrades();
+            Console.WriteLine($"OddThread: {i}");
+            Thread.Sleep(500);
         }
+
+    }
+}
+
+
+static void EvenThread()
+{
+    for (int i = 0; i <= 15; i++)
+    {
+        if (i % 2 != 0)
+        {
+            Console.WriteLine($"EvenThread: {i}");
+            Thread.Sleep(500);
+
+        }
+
     }
 }
