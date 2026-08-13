fd88ef2e774df099dabdd67c56d82451a4920736
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-08-13 21:16:17 +0400

    feat: create function ro check whether string is palindrome

--- Program.cs
+++ Program.cs
@@ -1,31 +1,29 @@
-﻿Thread odd = new(OddThread);
-//Thread even = new(EvenThread);
-
-odd.Start();
-odd.Join();
-
-Console.WriteLine("dasasruli");
-static void OddThread()
+﻿public static class StringExtension
 {
-    for (int i = 0; i <= 15; i++)
+    public static bool IsPalindrome(this string text)
     {
-        Console.WriteLine($"count: {i}");
-        Thread.Sleep(500);
+        for (int i = 0; i < text.Length; i++)
+        {
+            char left = text[i];
+            char right = text[text.Length - 1 - i];
 
+            if (left != right)
+                return false;
+        }
+
+        return true;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        string string1 = "Irakli";
+        string string2 = "abba";
+        string string3 = "abaa";
+        Console.WriteLine(string1.IsPalindrome() ? $"{string1} is Palindrome" : $"{string1} is not Palindrome");
+        Console.WriteLine(string2.IsPalindrome() ? $"{string2} is Palindrome" : $"{string2} is not Palindrome");
+        Console.WriteLine(string3.IsPalindrome() ? $"{string3} is Palindrome" : $"{string3} is not Palindrome");
     }
 }
 
-
-//static void EvenThread()
-//{
-//    for (int i = 0; i <= 15; i++)
-//    {
-//        if (i % 2 != 0)
-//        {
-//            Console.WriteLine($"EvenThread: {i}");
-//            Thread.Sleep(500);
-
-//        }
-
-//    }
-}
