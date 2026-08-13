0b5bf893a3a001503c8ee2b48c7e78613f3af00f
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-08-13 20:43:37 +0400

    feat: manipulate threads

--- Program.cs
+++ Program.cs
@@ -1,37 +1,31 @@
 ﻿Thread odd = new(OddThread);
-Thread even = new(EvenThread);
+//Thread even = new(EvenThread);
 
 odd.Start();
-even.Start();
-
 odd.Join();
-even.Join();
 
 Console.WriteLine("dasasruli");
 static void OddThread()
 {
     for (int i = 0; i <= 15; i++)
     {
-        if (i % 2 == 0)
-        {
-            Console.WriteLine($"OddThread: {i}");
-            Thread.Sleep(500);
-        }
+        Console.WriteLine($"count: {i}");
+        Thread.Sleep(500);
 
     }
 }
 
 
-static void EvenThread()
-{
-    for (int i = 0; i <= 15; i++)
-    {
-        if (i % 2 != 0)
-        {
-            Console.WriteLine($"EvenThread: {i}");
-            Thread.Sleep(500);
+//static void EvenThread()
+//{
+//    for (int i = 0; i <= 15; i++)
+//    {
+//        if (i % 2 != 0)
+//        {
+//            Console.WriteLine($"EvenThread: {i}");
+//            Thread.Sleep(500);
 
-        }
+//        }
 
-    }
+//    }
 }
